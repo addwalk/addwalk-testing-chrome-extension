ebd8e8f8268a71966650006dd813e2b9c4b10375
Frank Cieslik <frank.cieslik@gmail.com>
2014-09-29 13:31:45 +0200

    bugfixes, added storage-container for local-settings, make widget-embed dynamic

--- popup.js
+++ popup.js
@@ -1,83 +1,35 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2014 addwalk GmbH. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'kittens';
-
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
+var addwalkTestWidget = {
 
   /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
+   * Saves the Settings
    *
    * @public
    */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
+
+  saveSettings: function() {
+    // Get a value saved in a form.
+    var widget_id = document.getElementById("addwalk_settings_widget_id").value;
+    var widget_url = document.getElementById("addwalk_settings_widget_url").value;
+    // Save it using the Chrome local storage.
+    chrome.storage.local.set({ 'addwalk_settings_widget_id': widget_id, 'addwalk_settings_widget_url': widget_url }, function() {
+      //message('Settings saved');
+    });
   },
 
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
-
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
+  init: function() {
+    // set  click listener on Save-Button
+    document.getElementById("addwalk_settings_save").addEventListener('click', addwalkTestWidget.saveSettings, true);
+    // prefill value for widget-id
+    chrome.storage.local.get(['addwalk_settings_widget_id', 'addwalk_settings_widget_url' ], function(items){
+      document.getElementById("addwalk_settings_widget_id").value = items.addwalk_settings_widget_id;
+      document.getElementById("addwalk_settings_widget_url").value = items.addwalk_settings_widget_url;
+    });
   }
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
-});
+
+addwalkTestWidget.init();
--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var actualCode =  ["var addwalk_widget = addwalk_widget || {}; addwalk_widget['_id'] = 'XXXXXXXXXXXXXXXXXXX'; (function () { var _add = document.createElement('script'); _add.type = 'text/javascript';_add.async = true; _add.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'adds.addwalk.dev:3000/assets/adds/get_widget.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_add, s); })();",
-                  ' // Beware! This array have to be joined',
-                  ' // using a newline. Otherwise, missing semicolons',
-                  ' //  or single-line comments (//) will mess up your',
-                  ' //  code ----->'].join('\n');
+var actualCode =  [
+  "var addwalk_widget = addwalk_widget || {}; addwalk_widget['_id'] = '__widget_id__'; (function () { var _add = document.createElement('script'); _add.type = 'text/javascript';_add.async = true; _add.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'adds.addwalk.dev:3000/assets/adds/get_widget.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(_add, s); })();",
+].join('\n');
 
-var script = document.createElement('script');
-script.textContent = actualCode;
-(document.head||document.documentElement).appendChild(script);
-script.parentNode.removeChild(script);
+chrome.storage.local.get(['addwalk_settings_widget_id','addwalk_settings_widget_url'], function(items){
+  widget_id = items.addwalk_settings_widget_id;
+  widget_url = items.addwalk_settings_widget_url;
+  if(widget_id != "" && (document.URL.indexOf(widget_url) > -1)) {
+    console.log("addwalk-test-widget successfully initialized");
+    var script = document.createElement('script');
+    script.textContent = actualCode.replace("__widget_id__", widget_id);
+    (document.head||document.documentElement).appendChild(script);
+    script.parentNode.removeChild(script);
+  }
+});
